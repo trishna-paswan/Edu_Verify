9174fd39eba6abe2b310091b16df12fa5cfa2ec0
thepratikguptaa <pratikguptamain@gmail.com>
2025-06-01 16:21:29 +0530

    nextjs project

--- src/app/about/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function ChaiPage() {
-    return (
-        <h1 className="text-3xl font-bold text-center">My name is Pratik</h1>
-    )
-}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-
 export default function Home() {
   return (
     <h1 className="text-3xl font-bold text-center">Hello User</h1>
