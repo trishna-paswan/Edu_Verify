d4ac84537f9c26175c6c9a37465e48848e48d930
thepratikguptaa <pratikguptamain@gmail.com>
2025-06-01 21:41:15 +0530

    nextjs project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import NavBar from "@/components/NavBar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <h2>imagine this is a header</h2>
+        <NavBar />
         {children}
       </body>
     </html>
