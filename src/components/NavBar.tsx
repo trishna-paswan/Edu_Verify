d4b950b9e372a0131f26f45bf4a0f8d468c08f1e
thepratikguptaa <pratikguptamain@gmail.com>
2025-06-02 19:14:29 +0530

    nextjs project

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React, { useState } from "react";
-import { HoveredLink, Menu, MenuItem, ProductItem } from "./ui/navbar-menu";
+import { HoveredLink, Menu, MenuItem} from "./ui/navbar-menu";
 import { cn } from "@/utils/utils";
 import Link from "next/link";
 
@@ -14,14 +14,13 @@ function Navbar({ className }: { className?: string }) {
         <Menu setActive={setActive}>
             <Link href={"/"}>
             <MenuItem setActive={setActive} active={active} item="Home">
-            
             </MenuItem>
             </Link>
             <MenuItem
             setActive={setActive} active={active} item="Our Courses"
             >
-               <div className="flex flex-col space-y-4 text-sm">
-               <HoveredLink href="/courses">All Courses</HoveredLink>
+                <div className="flex flex-col space-y-4 text-sm">
+                <HoveredLink href="/courses">All Courses</HoveredLink>
             <HoveredLink href="/courses">
               Basic Music Theory
             </HoveredLink>
@@ -32,7 +31,7 @@ function Navbar({ className }: { className?: string }) {
             <HoveredLink href="/courses">
               Music Production
             </HoveredLink>
-               </div>
+                </div>
             </MenuItem>
             <Link href={"/contact"}>
             <MenuItem setActive={setActive} active={active} item="Contact Us">
