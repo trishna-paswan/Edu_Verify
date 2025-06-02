cc40e90534eb509dad1223b0a68bc0f53f0afc4a
thepratikguptaa <pratikguptamain@gmail.com>
2025-06-02 13:34:44 +0530

    nextjs project

---
+++ src/components/HeroSection.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function HeroSection() {
+  return (
+    <div
+    className='h-auto md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0'
+    >HeroSection</div>
+  )
+}
+
+export default HeroSection
