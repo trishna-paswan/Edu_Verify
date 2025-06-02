a3ad99b1a7bbc5755b08ece5b0951c67846393bb
thepratikguptaa <pratikguptamain@gmail.com>
2025-06-02 11:00:08 +0530

    nextjs project

--- src/components/ui/navbar-menu.tsx
+++ src/components/ui/navbar-menu.tsx
@@ -117,7 +117,7 @@ export const HoveredLink = ({
   return (
     <a
       {...rest}
-      className="text-neutral-700 dark:text-neutral-200 hover:text-black "
+      className="text-neutral-700 dark:text-neutral-200 hover:text-white "
     >
       {children}
     </a>
