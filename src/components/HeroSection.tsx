ba76f4ada58f66db2a99f3975a5cc4dd2dc26d84
Pratik Gupta <pratikguptamain@gmail.com>
2025-06-08 15:04:13 +0530

    nextjs project

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -6,7 +6,7 @@ import { Button } from "./ui/moving-border";
 function HeroSection() {
   return (
     <div
-    className='h-auto md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0'
+    className='h-auto md:h-[47rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0'
     >
         <Spotlight
         className="-top-40 left-0 md:-top-20 md:left-60"
