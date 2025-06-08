ecf8d78b3799e6f271b26d2ed3007fb7b4fd5c0f
Pratik Gupta <pratikguptamain@gmail.com>
2025-06-08 13:05:34 +0530

    nextjs project

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -10,7 +10,7 @@ function HeroSection() {
     >
         <Spotlight
         className="-top-40 left-0 md:-top-20 md:left-60"
-        fill="gold"
+        fill="white"
       />
         <div className='p-4 relative z-10 w-full text-center'>
             <h1 className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400">
