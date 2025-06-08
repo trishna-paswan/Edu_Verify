09d05bfaa989dd9cd361d53f62e16cbee1710d86
Pratik Gupta <pratikguptamain@gmail.com>
2025-06-09 01:08:27 +0530

    nextjs project

--- src/components/TestimonialCards.tsx
+++ src/components/TestimonialCards.tsx
@@ -1,4 +1,5 @@
 'use client'
+import { cn } from "@/utils/utils";
 import { InfiniteMovingCards } from "./ui/infinite-moving-cards";
 
 const musicSchoolTestimonials = [
@@ -35,8 +36,17 @@ const musicSchoolTestimonials = [
     ];
 function MusicSchoolTestimonials() {
     return (
-    <div className="h-[40rem] w-full bg-black bg-grid-white/[0.2] relative flex flex-col items-center justify-center overflow-hidden">
-        <h2 className="text-3xl font-bold text-center mb-8 z-10 text-neutral-50">Hear Our Harmony: Voices of Success</h2>
+    <div className="h-[47rem] w-full bg-black bg-grid-white/[0.2] relative flex flex-col items-center justify-center overflow-hidden">
+        <div
+        className={cn(
+            "absolute inset-0",
+            "[background-size:20px_20px]",
+            "[background-image:radial-gradient(#d4d4d4_1px,transparent_1px)]",
+            "dark:[background-image:radial-gradient(#404040_1px,transparent_1px)]",
+        )}
+        />
+        <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black"></div>
+        <h2 className="text-3xl font-bold text-center mb-8 z-10 text-neutral-50 m-6">Hear Our Harmony: Voices of Success</h2>
         <div className="flex justify-center w-full overflow-hidden px-4 sm:px-6 lg:px-8">
             <div className="w-full max-w-6xl">
             <InfiniteMovingCards
--- src/components/ui/infinite-moving-cards.tsx
+++ src/components/ui/infinite-moving-cards.tsx
@@ -6,7 +6,7 @@ import React, { useEffect, useState } from "react";
 export const InfiniteMovingCards = ({
   items,
   direction = "left",
-  speed = "fast",
+  speed = "slow",
   pauseOnHover = true,
   className,
 }: {
