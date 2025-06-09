f56f2aeaa2145b36396c5f4687f0b0e0fba2ecb3
Pratik Gupta <pratikguptamain@gmail.com>
2025-06-10 00:48:40 +0530

    nextjs project

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -36,7 +36,7 @@ function MusicSchoolContactUs() {
             value={email}
             onChange={(e) => setEmail(e.target.value)}
             placeholder="Your email address"
-            className="rounded-lg border border-neutral-800 focus:ring-2 focus:ring-teal-500 w-full p-4 bg-neutral-950 placeholder:text-neutral-700"
+            className="rounded-lg border border-neutral-800 focus:ring-2 focus:ring-teal-500 w-full p-4 bg-neutral-950 placeholder:text-neutral-700 text-white"
             required
           />
           <textarea
