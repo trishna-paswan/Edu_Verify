9fb544886b7663fb2828a7bcd34a5c0dbdb89fcf
Pratik Gupta <pratikguptamain@gmail.com>
2025-06-09 22:04:25 +0530

    nextjs project

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -80,7 +80,7 @@ function Footer() {
         </div>
                 <div></div>
             </div>
-            <p className="text-center text-xs pt-8">© 2024 Music School. All rights reserved.</p>
+            <p className="text-center text-xs pt-8">© 2025 Music School. All rights reserved.</p>
         </footer>
     )
 }
